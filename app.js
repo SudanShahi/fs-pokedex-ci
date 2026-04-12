ec89c3f5d8deda9915f6656a676991bfdf427da3
SudanShahi <Sudan.shahi@metropolia.fi>
2026-04-12 21:22:32 +0300

    fix: production express server for render

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
-const express = require("express");
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// get the port from env variable
-const PORT = process.env.PORT || 5001;
+app.get('/version', (req, res) => {
+  res.send('1')
+})
 
-app.use(express.static("dist"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')))
 
-const start = async () => {
-  await app.listen(PORT)
-  console.log(`server started on port ${PORT}`)
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'))
+  })
 }
 
-start()
+const PORT = process.env.PORT || 5001
+
+app.listen(PORT, () => {
+  console.log(`server started on port ${PORT}`)
+})
