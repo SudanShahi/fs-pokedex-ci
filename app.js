0b7139e6ea1b0d27fa6085cdd12ee205e4f8df16
SudanShahi <Sudan.shahi@metropolia.fi>
2026-04-12 21:34:33 +0300

    fix: express 5 wildcard route for render

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ app.get('/version', (req, res) => {
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'build')))
 
-  app.get('*', (req, res) => {
+  app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'))
   })
 }
